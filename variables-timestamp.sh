33e639c28d546566ed2e9c5076ae3d9f4fc6eb63
keerthi botlagunta <keerthibotlagunta28@gmail.com>
2026-01-29 20:13:43 -0500

    variables timestamp

--- variables-timestamp.sh
+++ variables-timestamp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-START_TIME=$(date + %s) # we put the command in between ()
+START_TIME=$(date +%s) # we put the command in between ()
 echo "Script executed at:$START_TIME"
 
 sleep 10
 
-END_TIME=$(dtae +$s)
+END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "Script executed in:$TOTAL_TIME"
