a9707d842865f9b3ac4cf030eb1ea0a45224abe0
keerthi botlagunta <keerthibotlagunta28@gmail.com>
2026-01-29 19:24:46 -0500

    variables

---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1=Sravani
+PERSON2=Keerthi
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1,I am fine. How are you doing?"
+echo "$PERSON1:: I am doing fine. what are you learning Ramesh?"
+echo "$PERSON2:: I am learning Shell"
