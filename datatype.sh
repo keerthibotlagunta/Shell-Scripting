d89b4bfc7e9ea85bed6a5ee0a4b2e88cf66f8048
keerthi botlagunta <keerthibotlagunta28@gmail.com>
2026-01-29 20:52:51 -0500

    data type example

--- datatype.sh
+++ datatype.sh
@@ -9,3 +9,6 @@ echo "Sum is : $SUM"
 #Array
 FRUITS=("Apple" "Banana" "carrot")
 echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
