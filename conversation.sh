a1462c575d27ac5b9ff4b153c72de6002c58496f
keerthi botlagunta <keerthibotlagunta28@gmail.com>
2026-01-29 19:16:08 -0500

    shell

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hello Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh,I am fine. How are you doing?"
+echo "Suresh:: I am doing fine. what are you learning Ramesh?"
+echo "Ramesh:: I am learning Shell"
